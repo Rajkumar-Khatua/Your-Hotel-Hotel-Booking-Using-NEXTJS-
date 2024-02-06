39cb98cec2e6f4dfad0130c88c59ed5795859299
Rajkumar-Khatua <rajkhatua2020@gmail.com>
2024-02-06 13:03:15 +0530

    Categories are adding

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
 import { SafeUser } from "@/app/types";
+import Categories from "./Categories";
 interface NavbarProps {
   currentUser?: SafeUser | null;
 }
@@ -22,6 +23,7 @@ export const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
           </div>
         </Container>
       </div>
+      <Categories />
     </div>
   );
 };
