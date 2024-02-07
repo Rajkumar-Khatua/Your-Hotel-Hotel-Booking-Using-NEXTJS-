edbbf6b6844bf5b5e83d85becaa22acfbe4f726e
Rajkumar-Khatua <rajkhatua2020@gmail.com>
2024-02-07 09:59:15 +0530

    Setup and works well clouidnary

---
+++ app/components/inputes/ImageUpload.tsx
@@ -0,0 +1,77 @@
+"use client";
+import { CldUploadWidget } from "next-cloudinary";
+import Image from "next/image";
+import { useCallback } from "react";
+import { TbPhotoPlus } from "react-icons/tb";
+
+declare global {
+  var cloudinary: any;
+}
+interface ImageUploadProps {
+  onChange: (value: string) => void;
+  value: string;
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({ onChange, value }) => {
+  const handleUpload = useCallback(
+    (result: any) => {
+      onChange(result.info.secure_url);
+    },
+    [onChange]
+  );
+
+  return (
+    <CldUploadWidget
+      onUpload={handleUpload}
+      uploadPreset="co3q4ixj"
+      options={{
+        maxFiles: 1,
+      }}
+    >
+      {({ open }) => {
+        return (
+          <div
+            onClick={() => open?.()}
+            className="relative
+                        cursor-pointer
+                        hover:opacity-70
+                        transition
+                        duration-200
+                        border-dashed
+                        border-2
+                        p-20
+                        border-neutral-300
+                        flex
+                        flex-col
+                        justify-center
+                        items-center
+                        gap-4
+                        text-neutral-600
+                "
+          >
+            <TbPhotoPlus size={50} />
+            <div
+              className="
+                        font-semibold
+                        text-lg            "
+            >
+              Click here to Upload Image
+            </div>
+            {value && (
+              <div className="absolute inset-0 w-full h-full">
+                <Image
+                  alt="Upload"
+                  fill
+                  style={{ objectFit: "cover" }}
+                  src={value}
+                />
+              </div>
+            )}
+          </div>
+        );
+      }}
+    </CldUploadWidget>
+  );
+};
+
+export default ImageUpload;
--- app/components/models/RentModel.tsx
+++ app/components/models/RentModel.tsx
@@ -10,6 +10,7 @@ import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputes/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "../inputes/Counter";
+import ImageUpload from "../inputes/ImageUpload";
 
 enum STEPS {
   CATEGORY = 0,
@@ -50,6 +51,7 @@ const RentModel = () => {
   const guestCount = watch("guestCount");
   const roomCount = watch("roomCount");
   const bathroomCount = watch("bathroomCount");
+  const imageSrc = watch("imageSrc");
 
   const Map = useMemo(
     () =>
@@ -155,6 +157,22 @@ const RentModel = () => {
           subtitle="How many bathrooms do you have?"
           value={bathroomCount}
           onChange={(value) => setCustomValue("bathroomCount", value)}
+        />
+      </div>
+    );
+  }
+
+  // Images Step 4
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Upload images of your place"
+          subtitle="Show off your place to your guests"
+        />
+        <ImageUpload
+          value={imageSrc}
+          onChange={(value) => setCustomValue("imageSrc", value)}
         />
       </div>
     );
